6ad2b4306e9db90b5e2583f3a623ccb2578e2423
tanvilalsare <tanvilalsare09@gmail.com>
2026-07-07 22:10:15 +0530

    Time: 47 ms (47.82%), Space: 54.2 MB (43.16%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,9 @@
+var reduce = function(nums, fn, init) {
+    let result = init;
+
+    for (let i = 0; i < nums.length; i++) {
+        result = fn(result, nums[i]);
+    }
+
+    return result;
+};
