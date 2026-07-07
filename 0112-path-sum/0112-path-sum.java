a479df0f00dc21bfc5e66033c1806f4858af7213
tanvilalsare <tanvilalsare09@gmail.com>
2026-07-07 22:07:27 +0530

    Time: 0 ms (100%), Space: 45.1 MB (31.64%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        if (root == null) {
+            return false;
+        }
+
+        // If it's a leaf node
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+
+        return hasPathSum(root.left, targetSum - root.val) ||
+               hasPathSum(root.right, targetSum - root.val);
+    }
+}
