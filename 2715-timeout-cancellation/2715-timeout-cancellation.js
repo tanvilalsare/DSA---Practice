9e5b7646ccad2fddcc4a51bd5af9f4d496da5321
tanvilalsare <tanvilalsare09@gmail.com>
2026-07-09 21:43:09 +0530

    Time: 56 ms (58.45%), Space: 53.7 MB (79.18%) - LeetHub

---
+++ 2715-timeout-cancellation/2715-timeout-cancellation.js
@@ -0,0 +1,9 @@
+var cancellable = function(fn, args, t) {
+    const timer = setTimeout(() => {
+        fn(...args);
+    }, t);
+
+    return function cancelFn() {
+        clearTimeout(timer);
+    };
+};
