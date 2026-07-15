b40f0a83622dbf57ac5028f7ff75519c6e1b9386
tanvilalsare <tanvilalsare09@gmail.com>
2026-07-15 16:01:55 +0530

    Time: 0 ms (100%), Space: 42.2 MB (38.77%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -5,9 +5,9 @@ class Solution {
         while(s<=e){
             int mid = s+(e-s)/2;
             long sq = (long)mid*mid;
-            if(sq>num){
+            if(sq>num){// sq is big i.e we need smaller range so e = mid-1
                 e = mid-1;
-            }else if(sq<num){
+            }else if(sq<num){ // sq is smaller i.e we need larger range so s = mid+1
                 s = mid+1;
             }else {
                 return true;
