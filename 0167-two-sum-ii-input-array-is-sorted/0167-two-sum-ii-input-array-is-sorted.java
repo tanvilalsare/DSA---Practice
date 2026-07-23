9668ff8d614c33e48d1f629cf73bec33a94da98a
tanvilalsare <tanvilalsare09@gmail.com>
2026-07-23 11:08:59 +0530

    Time: 2 ms (95.57%), Space: 48.4 MB (42.12%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,13 +1,15 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int n = numbers.length;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int sum = numbers[i]+numbers[j];
-                if(sum == target){
-                    return new int []{i+1,j+1};
-                }
-                                       
+        int low=0;
+        int high= numbers.length-1;
+        while(low<=high){
+            int sum = numbers[high]+numbers[low];
+            if(sum == target){
+                return new int[]{low+1,high+1};//+1 as in o/p we want index from 1 not 0
+            }else if(sum<target){//we need biger number sum 
+                low++;
+            }else{
+                high--; // sum>target : need smaller number for sum 
             }
         }
         return new int[]{-1,-1};
