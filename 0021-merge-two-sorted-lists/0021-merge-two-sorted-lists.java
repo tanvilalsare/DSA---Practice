bedf01c31be15dace95ebc1670840ddbc83c615f
tanvilalsare <tanvilalsare09@gmail.com>
2026-07-28 15:04:33 +0530

    Time: 0 ms (100%), Space: 44.5 MB (20.78%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,24 +10,19 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-       
-        ListNode dummy = new ListNode(-1);
-        ListNode current = dummy;
-
-        while (list1 != null && list2 != null) {
-            if (list1.val <= list2.val) {
-                current.next = list1;
-                list1 = list1.next; // Move forward in list1
-            } else {
-                current.next = list2;
-                list2 = list2.next; // Move forward in list2
-            }
-            current = current.next; // Move forward in the merged list
+       ListNode dummy = new ListNode(0);
+       ListNode curr= dummy ;
+       while(list1!=null && list2!= null){
+        if(list1.val<=list2.val){
+            curr.next = list1;// move dummy to list1
+            list1=list1.next;// mover list1 ahead to .next
+        }else{
+            curr.next = list2;
+            list2= list2.next;
         }
-
-        
-        current.next = (list1 != null) ? list1 : list2;
-
-        return dummy.next;
+        curr = curr.next; // let curr move hence curr = curr.next
+       }// for handling those case where our loop breaks when either list1 or list2 = null so we will then just add the next element of the remaing list as it is as its an sorted list 
+       curr.next =list1!=null ? list1 : list2;// is L1 is nul or L2 
+       return dummy.next;  // .next cause it starts with 0 and 0 is not part of the list and return dummy not curr as curr moves with the merge but dummy dosent move 
     }
 }
