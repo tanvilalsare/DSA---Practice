f6597e346957370832e20456edb74f219572f8ea
tanvilalsare <tanvilalsare09@gmail.com>
2026-08-03 14:05:16 +0530

    Time: 370 ms (48.61%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -4,4 +4,4 @@ FROM Employee e1
 LEFT JOIN Employee e2
    ON e1.id = e2.managerId -- e1:the employe e2: whom the employee report too 
 GROUP BY e1.id , e1.name
-HAVING COUNT(e2.id)>=5;
+HAVING COUNT(e2.id)>=5; -- managers who have 5 or more direct reports.
