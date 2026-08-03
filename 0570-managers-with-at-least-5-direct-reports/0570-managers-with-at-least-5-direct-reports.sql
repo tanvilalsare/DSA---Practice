e5ec2f6fa0dd7ade3786068186b42258c9409fa7
tanvilalsare <tanvilalsare09@gmail.com>
2026-08-03 14:04:44 +0530

    Time: 421 ms (17.63%), Space: 0B (100%) - LeetHub

---
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT e1.name
+FROM Employee e1
+LEFT JOIN Employee e2
+   ON e1.id = e2.managerId -- e1:the employe e2: whom the employee report too 
+GROUP BY e1.id , e1.name
+HAVING COUNT(e2.id)>=5;
