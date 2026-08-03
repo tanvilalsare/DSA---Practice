e1b767cb36b84ac311819c696b1c75b76cfe0724
tanvilalsare <tanvilalsare09@gmail.com>
2026-08-03 14:35:17 +0530

    Time: 732 ms (39.58%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -2,7 +2,7 @@
 SELECT s.user_id,
        ROUND(
         IFNULL( 
-        SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) /COUNT(c.action) 
+        SUM(CASE WHEN c.action = 'confirmed' THEN 1 ELSE 0 END) /COUNT(c.action) -- IFNULL(value, fallback) value is not null and 0 is null 
         ,0) ,2) AS confirmation_rate
 FROM Signups s
 LEFT JOIN Confirmations c
