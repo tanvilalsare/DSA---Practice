00c1deb62d227180d73c8357f16ec81798ee9cb8
tanvilalsare <tanvilalsare09@gmail.com>
2026-08-06 12:17:20 +0530

    Time: 3 ms (74.14%), Space: 47.7 MB (67.66%) - LeetHub

--- 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
+++ 0989-add-to-array-form-of-integer/0989-add-to-array-form-of-integer.java
@@ -3,22 +3,22 @@ class Solution {
         List<Integer> ans = new ArrayList<>();
         int i = num.length-1; // to point at the last elemnt as we add from the back 
         int carry =0;
-        while(i>=0 || k>0){
-            int sum = carry;;
+        while(i>=0 || k>0){ // till digits are in bound 
+            int sum = carry;
             if(i>=0){
                 sum += num[i--];// as we are going back to front
             }
             if(k>0){
-                sum+=k%10;
-                k/= 10;
+                sum+=k%10;// add the digit 
+                k/= 10;// revove the digit from the number
             }
             ans.add(sum%10);// to get the carry out 
             carry = sum/10;
         }
         if(carry>0){
-            ans.add(carry);
+            ans.add(carry);// at last if any carry left add that 
         }
-        Collections.reverse(ans);
+        Collections.reverse(ans);// as we are adding from back we get the ans in revers order so reverse it to get the actual ans
         return ans;
     }
 }
