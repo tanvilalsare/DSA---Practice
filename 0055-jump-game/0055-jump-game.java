a3dd83815b805fa0af9d3c2b2f1bbed5b1f47b73
tanvilalsare <tanvilalsare09@gmail.com>
2026-08-12 13:41:54 +0530

    Time: 2 ms (88.81%), Space: 47.7 MB (81.72%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int farthest = 0;
+        for(int i =0;i<n;i++){
+            if(i>farthest){
+                return false;
+            }
+            farthest = Math.max(i+nums[i] , farthest);
+        }
+        return true;
+    }
+}
