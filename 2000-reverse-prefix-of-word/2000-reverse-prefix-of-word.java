dbe815b227566f6235c6d5c28dcb3730cb7e4661
tanvilalsare <tanvilalsare09@gmail.com>
2026-08-26 13:45:15 +0530

    Time: 0 ms (100%), Space: 43 MB (55.09%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -1,7 +1,7 @@
 class Solution {
     public String reversePrefix(String word, char ch) {
         int idx = word.indexOf(ch);//in-built fnx to ge index of that character from the string by scanning it internally 
-        if(ch == -1){
+        if(idx == -1){
             return word;
         }
         String prefix = word.substring(0 , idx+1);//(start , end) end is not included so we take +1 as if we just take idx it won't be included ; end is exclusive 
